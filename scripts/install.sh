288e758cbcdc71c262f11ef8b35ae46109cc99a3
Enrico Rijken <enricorijken@gmail.com>
2026-09-03 15:17:11 +0200

    Clone ReSide repo from default branch

--- scripts/install.sh
+++ scripts/install.sh
@@ -11,9 +11,10 @@ if [ -d .git ]; then
   git fetch --depth 1 origin main
   git reset --hard origin/main
 else
-  git clone --depth 1 --branch main https://github.com/EnricoRijken/reside-pterodactyl.git .
+  git clone --depth 1 https://github.com/EnricoRijken/reside-pterodactyl.git .
 fi
 
 chmod +x start.sh stop.sh start.ps1 stop.ps1 start.bat stop.bat       CollectLogs.sh CollectLogs.ps1 CollectLogs.bat       Api/run.sh Api/stop.sh Api/reset-database.sh       scripts/entrypoint.sh
 
 mkdir -p logs
+
